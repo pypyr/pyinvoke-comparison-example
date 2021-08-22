c8a814f5a73164d480a006e9b05c8f285892cfd3
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2021-08-22 17:20:54 +0200

    read package version dynamically

--- arbpackage/version.py
+++ arbpackage/version.py
@@ -2,16 +2,24 @@
 
 import platform
 
-__version__ = '0.0.11'
+
+def package_version():
+    """Return version of this package."""
+    package_name = "arbpackage"
+    import pkg_resources
+
+    return pkg_resources.get_distribution(package_name).version
+
+
+__version__ = package_version()
 
 
 def get_version():
     """Return package-name __version__ python python_version."""
-    return (f'arbpackage {__version__} '
-            f'python {platform.python_version()}')
+    return f"arbpackage {__version__} " f"python {platform.python_version()}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Entry point for script execution.
 
     Makes it easy to get version number from cli from outside the package.
--- tests/unit/version_test.py
+++ tests/unit/version_test.py
@@ -1,11 +1,21 @@
 """version.py unit tests."""
-import arbpackage.version
 import platform
+
+import arbpackage.version
 
 
 def test_get_version():
     """Version is as expected."""
     actual = arbpackage.version.get_version()
-    expected = (f'arbpackage {arbpackage.version.__version__} '
-                f'python {platform.python_version()}')
+    expected = (
+        f"arbpackage {arbpackage.version.__version__} "
+        f"python {platform.python_version()}"
+    )
     assert actual == expected, "version not returning correctly"
+
+
+def test_package_version():
+    """Package version by package_version() shall equal __version__."""
+    by_var = arbpackage.version.__version__
+    by_func = arbpackage.version.package_version()
+    assert by_var == by_func
